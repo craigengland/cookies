3cd0193863da05e7c5970d3a73ac04aa7bcf1529
Craig England <craig.england@krystal.uk>
2021-03-25 14:54:21 +0000

    tweak: change the Crumbs to a class

--- src/main.js
+++ src/main.js
@@ -1,18 +1,18 @@
 import { pubsub } from './components/pubsub';
 import './main.css';
 
-const Crumbs = {
-  cookies: [],
-  state: {
-    functional: false,
-    performance: false,
-    targeting: false,
-  },
+class Crumbs {
+  constructor() {
+    this.cookies = [];
+    this.banner = null;
+    this.editScreen = null;
+    this.render();
+  }
+
   render() {
     // Render the Cookie banner if analytics haven't been accepted
     // and subscribe to some events
-
-    if (!Crumbs.getCookie('cookie_consent')) {
+    if (!this.getCookie('cookie_consent')) {
       // Create the banner itself as a template literal and add it
       // to the DOM, at the end of the body
       const cookieBanner = `
@@ -28,17 +28,22 @@ const Crumbs = {
         </div>
       `;
       document.body.insertAdjacentHTML('beforeend', cookieBanner);
-      Crumbs.editSettings();
+
+      // Set the banner property so we have access to remove it later
+      this.banner = document.querySelector('.crumbs-banner');
+
+      this.editSettings();
       pubsub.subscribe('cookiesUpdated', (cookie) => {
         window.confirm(`${cookie.name}, ${cookie.value} has been added`);
       });
     }
-  },
+  }
 
   editSettings() {
     const editSettingsButtons = document.querySelector('.crumbs-edit-settings');
     const editScreen = `
       <div class="crumbs-edit">
+        <button class="crumbs-edit-close">Close</button>
         <h4>Edit cookie settings</h4>
         <p>Check the cookies you want to accept</p>
         <div>
@@ -53,13 +58,41 @@ const Crumbs = {
           <label for="targeting">Targeting</label>
           <input type="checkbox" id="targeting" />
         </div>
-        <button class="crumbs-edit-accept>Set cookie preferences</button>
+        <button class="crumbs-edit-accept" style="margin: 1rem 0">Set cookie preferences</button>
       </div>
     `;
     editSettingsButtons.addEventListener('click', (e) => {
       document.body.insertAdjacentHTML('beforeend', editScreen);
+      this.editAccept();
+      this.closeEditScreen();
+      // Set the editScreen property so we have access to hide it later on.
+      this.editScreen = document.querySelector('.crumbs-edit');
     });
-  },
+  }
+
+  closeEditScreen() {
+    const editScreen = document.querySelector('.crumbs-edit');
+    const editClose = document.querySelector('.crumbs-edit-close');
+    editClose.addEventListener('click', () => {
+      editScreen.remove();
+    });
+  }
+
+  editAccept() {
+    const editAccept = document.querySelector('.crumbs-edit-accept');
+    editAccept.addEventListener('click', () => {
+      const functionalCheck = document.querySelector('#functional').checked;
+      const performanceCheck = document.querySelector('#performance').checked;
+      const targetingCheck = document.querySelector('#targeting').checked;
+
+      this.removeBanner(this.editScreen);
+      this.removeBanner(this.banner);
+
+      // Based on the selected checkboxes we will add the relevant cookies
+      // this.addCookies('functional');
+      console.log(`add preferences here to ${this.cookies}`);
+    });
+  }
 
   getCookie(cookieName) {
     const name = cookieName + '=';
@@ -79,34 +112,31 @@ const Crumbs = {
     }
 
     return '';
-  },
+  }
 
-  acceptAll(cookies) {
-    let cookiesList = new Set(Crumbs.cookies);
+  addCookies(cookies) {
+    let cookiesList = new Set(this.cookies);
 
     cookies.forEach((cookie) => {
-      Crumbs.setCookie(cookie.name, cookie.value, 365);
+      this.setCookie(cookie.name, cookie.value, 365);
       cookiesList.add(cookie);
       pubsub.publish('cookiesUpdated', cookie);
     });
 
-    Crumbs.cookies = Array.from(cookiesList);
+    this.cookies = Array.from(cookiesList);
+    this.setAcceptanceCookie();
+    this.removeBanner(this.banner);
+  }
 
-    Crumbs.state = {
-      functional: true,
-      performance: true,
-      targeting: true,
-    };
+  setAcceptanceCookie() {
+    // This hides the cookie banner if we want to select some cookies
+    this.setCookie('cookie_consent', true, 365);
+  }
 
-    // Set the overall consent to true if we accept all
-    Crumbs.setCookie('cookie_consent', true, 1);
-    Crumbs.removeBanner();
-  },
-
-  removeBanner() {
-    const cookieBanner = document.querySelector('.crumbs-banner');
-    cookieBanner.remove();
-  },
+  removeBanner(banner) {
+    banner.remove();
+    pubsub.unsubscribe('cookiesUpdated');
+  }
 
   setCookie(name, value, days) {
     let maxAge = '';
@@ -115,24 +145,30 @@ const Crumbs = {
       maxAge = `; max-age=${time}`;
     }
     document.cookie = `${name}=${value || ''}${maxAge}; path=/`;
-  },
-};
+  }
+}
 
 // This is us consuming the library in a project
 
 document.addEventListener('DOMContentLoaded', () => {
-  Crumbs.render();
-
   // A made up array of cookies that we are going to set when the
   // Accept all button is pressed
+  const c = new Crumbs();
   const cookies = [
     {
       name: '_ga',
       value: 'skufksdbfk234234',
+      type: 'functional',
     },
     {
       name: '_fb',
       value: 'slanfisdnifndiosnfon',
+      type: 'performance',
+    },
+    {
+      name: '_mclarity',
+      value: '1231412ddwq21dqeqwe',
+      type: 'targeting',
     },
   ];
 
@@ -141,7 +177,7 @@ document.addEventListener('DOMContentLoaded', () => {
   if (acceptCookies) {
     acceptCookies.addEventListener('click', (e) => {
       e.preventDefault();
-      Crumbs.acceptAll(cookies);
+      c.addCookies(cookies);
     });
   }
 });
